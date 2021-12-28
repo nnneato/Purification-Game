692fd7fa22748b29a712953a9d9207468207abc5
Asheton S. M <AshetonSM@gmail.com>
2021-12-28 14:12:28 -0600

    Added new furniture but excluded models from commit. Created (temp?) solution for flashlight aiming.

--- Assets/Scripts/FlashlightMovement.cs
+++ Assets/Scripts/FlashlightMovement.cs
@@ -1,40 +1,61 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
-
 
 public class FlashlightMovement : MonoBehaviour
 {
+    //[SerializeField] private Animator animator;
+    //[SerializeField] float speed = 200;
+    private float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    private Transform cam;
+    private Camera camComponent;
+    private Vector3 direction;
+    private RaycastHit hit;
+    int layermask = 1 << 8;
 
-    public Camera cam;
-    float mouseX;
-    float mouseY;
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
+        cam = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        camComponent = cam.GetComponent<Camera>();
+        //animator = GetComponentInChildren<Animator>();
 
+    }
     // Update is called once per frame
     void Update()
     {
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit raycastHit))
+
+        Ray ray = camComponent.ScreenPointToRay(Input.mousePosition);
+
+
+
+        // set directional input values
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        // set the direction by creating a new vector3
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        // Turn flashlight on or off
+        if (Input.GetButton("Fire1"))
         {
-            transform.LookAt(raycastHit.point);
+            Debug.Log("Fire1");
+        }
+
+        // If player is moving in any direction
+        if (direction.magnitude >= 0.1f)
+        {
+            // Rotate based on camera position
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        }
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layermask))
+        {
+            transform.LookAt(hit.point);
         }
 
     }
-
-    void RotateFlashlight(Vector3 mousePos)
-    {
-
-
-    }
-
 }
-
 
--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -4,48 +4,36 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
-    public Rigidbody playerRB;
-    public float speed = 200;
-    public Animator animator;
-    public float turnSmoothTime = 0.1f;
+    [SerializeField] private Rigidbody playerRB;
+    [SerializeField] private Animator animator;
+    [SerializeField] float speed = 200;
+    private float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
-    public Transform cam;
-    private bool carrying;
-    private bool running;
+    private Transform cam;
     private Vector3 direction;
 
 
     private void Start()
     {
-        carrying = false;
-        running = false;
+        cam = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        animator = GetComponentInChildren<Animator>();
+        playerRB = GetComponent<Rigidbody>();
+
     }
     // Update is called once per frame
     void Update()
     {
+
         // set directional input values
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         // set the direction by creating a new vector3
         direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        // Check if player is carrying something
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            IsCarrying(carrying);
-        }
-
-        // Check if player is running
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            RunningCheck(running);
-        }
-
         // If player is moving in any direction
         if (direction.magnitude >= 0.1f)
         {
             animator.SetBool("walk", true);
-
 
             // Rotate based on camera position
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -60,47 +48,8 @@ public class ThirdPersonMovement : MonoBehaviour
         {
             animator.SetBool("walk", false);
             animator.SetBool("run", false);
-            running = false;
         }
 
-
-
-        void IsCarrying(bool answer)
-        {
-            if (answer != true)
-            {
-                speed = 150;
-                animator.SetLayerWeight(1, 1);
-                carrying = true;
-                Debug.Log(carrying);
-            }
-            if (answer != false)
-            {
-                speed = 200;
-                animator.SetLayerWeight(1, 0);
-                carrying = false;
-                Debug.Log(carrying);
-            }
-        }
-
-
-        // Bug/feature: You keep running even if you let go of shift...
-        void RunningCheck(bool answer)
-        {
-            if (answer != true)
-            {
-                speed = 400;
-                animator.SetBool("run", true);
-                running = true;
-            }
-            else
-            {
-                speed = 200;
-                animator.SetBool("run", false);
-                running = false;
-            }
-
-        }
     }
 }
 
